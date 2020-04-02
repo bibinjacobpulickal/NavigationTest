2c2b77ae5a4a7c8de91f0d820344de589e5e3aac
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-04-02 21:25:12 +0530

    Added navigate.

--- NavigationTest/ViewController.swift
+++ NavigationTest/ViewController.swift
@@ -12,9 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .magenta
+        navigationItem.title = "First"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(navigate))
     }
 
+    @objc func navigate() {
+        navigationController?.pushViewController(SecondViewController(), animated: true)
+    }
+}
 
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+        navigationItem.title = "Second"
+    }
 }
 
